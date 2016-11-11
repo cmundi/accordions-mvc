8a80f36666e28a614d876dbb693a6c2848095206
C. Mundi <cmundi@gmail.com>
2016-11-10 23:25:44 -0700

    Add files via upload

---
+++ AngularUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularUI.Startup))]
+namespace AngularUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
